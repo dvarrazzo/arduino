905cdd7dadc977f9b80ada5e98812353471b16a4
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2012-04-15 00:05:11 +0100

    sercat -- read from stdin and send what read to the serial

--- tools/sercat.py
+++ tools/sercat.py
@@ -1,12 +1,50 @@
 #!/usr/bin/env python
 
 import sys
+import Queue
 import serial
+import threading
+from select import select
+
+def input_read(queue):
+    """Read from stdin and put the lines read into the queue *q*."""
+    while 1:
+        line = sys.stdin.readline()
+        if not line:
+            break
+
+        queue.put(line, block=False)
 
 def main():
+    # data to send to the serial
+    q = Queue.Queue()
+
+    # set the serial non-blocking to allow use with select
     ser = serial.Serial('/dev/ttyUSB0', 19200)
+    ser.nonblocking()
+
+    # receive data from stdin asynchronously
+    t = threading.Thread(target=input_read, args=(q,))
+    t.setDaemon(True)
+    t.start()
+
+    # NOTE: data is sent to the serial every second, not immediately.
+    # to send it immediately I should wait on stdin too, but it seems tricky
+    # in Python.
     while 1:
-        sys.stdout.write(ser.read())
+        # Check if there is data to read from the serial
+        rlist, _, _ = select([ser], [], [], 1)
+        if ser in rlist:
+            sys.stdout.write(ser.read())
+
+        # If there is a command, send it to the serial.
+        try:
+            cmd = q.get(block=False)
+        except Queue.Empty:
+            pass
+        else:
+            ser.write(cmd)
+
 
 if __name__ == '__main__':
     try:
