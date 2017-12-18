937ca07a0b1bd5c7738ab342c64784de509441a0
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2017-12-18 19:08:24 +0100

    Avoid the creation of a program.cpp

--- tools/rules.mk
+++ tools/rules.mk
@@ -9,7 +9,7 @@ ARDUINO_VARIANT ?= standard
 ARDUINO_LIBS_DIR = $(INSTALL_DIR)/libraries/
 AVR_TOOLS_PATH = /usr/bin
 
-CXXSRC += applet/program.cpp
+CXXSRC += $(TARGET).cpp
 
 # Arduino library required by about all the projects.
 
@@ -21,7 +21,8 @@ SRC += $(ARDUINO)/wiring_shift.c $(ARDUINO)/WInterrupts.c \
        $(ARDUINO)/wiring_analog.c
 
 CXXSRC += $(ARDUINO)/HardwareSerial.cpp $(ARDUINO)/WMath.cpp \
-          $(ARDUINO)/Print.cpp $(ARDUINO)/Stream.cpp $(ARDUINO)/new.cpp
+          $(ARDUINO)/Print.cpp $(ARDUINO)/Stream.cpp \
+          $(ARDUINO)/main.cpp $(ARDUINO)/new.cpp
 
 
 #
@@ -89,7 +90,7 @@ LDFLAGS += -Wl,--gc-sections
 
 # Programming support using avrdude. Settings and variables.
 AVRDUDE_PORT = $(PORT)
-AVRDUDE_WRITE_FLASH = -U flash:w:applet/$(TARGET).hex
+AVRDUDE_WRITE_FLASH = -U flash:w:$(TARGET).hex
 AVRDUDE_FLAGS = -V -F -C $(INSTALL_DIR)/hardware/tools/avrdude.conf \
 -p $(MCU) -P $(AVRDUDE_PORT) -c $(AVRDUDE_PROGRAMMER)
 
@@ -119,38 +120,30 @@ ALL_ASFLAGS = -mmcu=$(MCU) -I. -x assembler-with-cpp $(ASFLAGS)
 
 
 # Default target.
-all: applet_files build sizeafter
+all: build sizeafter
 
 build: elf hex
 
-# Here is the "preprocessing".
-# It creates a .cpp file including the project file and the other parts required
-# to have the libraries and a main() function.
-applet/program.cpp: $(TARGET).cpp
-	mkdir -p applet
-	echo "#include \"../$(TARGET).cpp\"" > $@
-	echo "#include \"$(ARDUINO)/main.cpp\"" >> $@
-
-elf: applet/$(TARGET).elf
-hex: applet/$(TARGET).hex
-eep: applet/$(TARGET).eep
-lss: applet/$(TARGET).lss
-sym: applet/$(TARGET).sym
+elf: $(TARGET).elf
+hex: $(TARGET).hex
+eep: $(TARGET).eep
+lss: $(TARGET).lss
+sym: $(TARGET).sym
 
 # Program the device.
-upload: applet/$(TARGET).hex
+upload: $(TARGET).hex
 	stty -F $(PORT) hupcl
 	$(AVRDUDE) $(AVRDUDE_FLAGS) $(AVRDUDE_WRITE_FLASH)
 
 
 # Display size of file.
-HEXSIZE = $(SIZE) --target=$(FORMAT) applet/$(TARGET).hex
-ELFSIZE = $(SIZE)  applet/$(TARGET).elf
+HEXSIZE = $(SIZE) --target=$(FORMAT) $(TARGET).hex
+ELFSIZE = $(SIZE)  $(TARGET).elf
 sizebefore:
-	@if [ -f applet/$(TARGET).elf ]; then echo; echo $(MSG_SIZE_BEFORE); $(HEXSIZE); echo; fi
+	@if [ -f $(TARGET).elf ]; then echo; echo $(MSG_SIZE_BEFORE); $(HEXSIZE); echo; fi
 
 sizeafter:
-	@if [ -f applet/$(TARGET).elf ]; then echo; echo $(MSG_SIZE_AFTER); $(HEXSIZE); echo; fi
+	@if [ -f $(TARGET).elf ]; then echo; echo $(MSG_SIZE_AFTER); $(HEXSIZE); echo; fi
 
 
 # Convert ELF to COFF for use in debugging / simulating in AVR Studio or VMLAB.
@@ -161,12 +154,12 @@ COFFCONVERT=$(OBJCOPY) --debugging \
 --change-section-address .eeprom-0x810000
 
 
-coff: applet/$(TARGET).elf
-	$(COFFCONVERT) -O coff-avr applet/$(TARGET).elf $(TARGET).cof
+coff: $(TARGET).elf
+	$(COFFCONVERT) -O coff-avr $(TARGET).elf $(TARGET).cof
 
 
 extcoff: $(TARGET).elf
-	$(COFFCONVERT) -O coff-ext-avr applet/$(TARGET).elf $(TARGET).cof
+	$(COFFCONVERT) -O coff-ext-avr $(TARGET).elf $(TARGET).cof
 
 
 .SUFFIXES: .elf .hex .eep .lss .sym
@@ -187,11 +180,11 @@ extcoff: $(TARGET).elf
 	$(NM) -n $< > $@
 
 # Link: create ELF output file from library.
-applet/$(TARGET).elf: applet/program.o applet/core.a
-	$(CC) $(ALL_CFLAGS) -o $@ applet/program.o -L. applet/core.a $(LDFLAGS)
+$(TARGET).elf: $(TARGET).o core.a
+	$(CC) $(ALL_CFLAGS) -o $@ $(TARGET).o -L. core.a $(LDFLAGS)
 
-applet/core.a: $(OBJ)
-	@for i in $(OBJ); do echo $(AR) rcs applet/core.a $$i; $(AR) rcs applet/core.a $$i; done
+core.a: $(OBJ)
+	@for i in $(OBJ); do echo $(AR) rcs core.a $$i; $(AR) rcs core.a $$i; done
 
 
 
@@ -227,12 +220,11 @@ applet/core.a: $(OBJ)
 
 # Target: clean project.
 clean:
-	$(REMOVE) applet/$(TARGET).hex applet/$(TARGET).eep applet/$(TARGET).cof applet/$(TARGET).elf \
-	applet/$(TARGET).map applet/$(TARGET).sym applet/$(TARGET).lss applet/core.a \
-	$(OBJ) $(LST) $(SRC:.c=.s) $(SRC:.c=.d) $(CXXSRC:.cpp=.s) $(CXXSRC:.cpp=.d) \
-	applet/program.cpp
+	$(REMOVE) $(TARGET).hex $(TARGET).eep $(TARGET).cof $(TARGET).elf \
+	$(TARGET).map $(TARGET).sym $(TARGET).lss core.a \
+	$(OBJ) $(LST) $(SRC:.c=.s) $(SRC:.c=.d) $(CXXSRC:.cpp=.s) $(CXXSRC:.cpp=.d)
 
-.PHONY:	all build elf hex eep lss sym program coff extcoff clean applet_files sizebefore sizeafter
+.PHONY:	all build elf hex eep lss sym coff extcoff clean sizebefore sizeafter
 
 include $(SRC:.c=.d)
 include $(CXXSRC:.cpp=.d)
