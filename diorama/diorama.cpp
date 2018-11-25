09c57449e76877c20dac8f1aef902c1c0718bd1d
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2018-11-25 21:13:35 +0000

    Diorama scene finished

--- diorama/diorama.cpp
+++ diorama/diorama.cpp
@@ -1,83 +1,103 @@
-#include "FastLED.h"
+#include <FastLED.h>
 
 static const int NLEDS = 100;
-static const int DATA_PIN = 5;
 CRGB leds[NLEDS];
 
-int led_state = 0;
+static const int DATA_PIN = 5;
 
-void scene();
+class Scene {
+public:
+    CHSV sky;
+    CHSV water;
+    CHSV ground;
+
+    Scene(long sky, long water, long ground)
+        :
+        sky(rgb2hsv_approximate(sky)),
+        water(rgb2hsv_approximate(water)),
+        ground(rgb2hsv_approximate(ground))
+    { }
+
+    Scene(CHSV &sky, CHSV &water, CHSV &ground)
+        : sky(sky), water(water), ground(ground)
+    {}
+
+    Scene(Scene &s1, Scene &s2, fract8 alpha)
+        :   sky(blend(s1.sky, s2.sky, alpha)),
+            water(blend(s1.water, s2.water, alpha)),
+            ground(blend(s1.ground, s2.ground, alpha))
+        {}
+};
+
+Scene white(0xffffff, 0xffffff, 0xffffff);
+Scene day(0x59a8ff, 0x1f604a, 0xff8080);
+Scene dusk(0x371b5b, 0x0e242b, 0xb28b55);
+Scene night(0x0e0e26, 0x000000, 0xbbffff);
+
+int times[] = {0, 20, 30, 40, 50, 60, 70, 80};
+Scene scenes[] = {white, white, day, day, dusk, night, night, white};
+int NEVENTS = sizeof(times) / sizeof(times[0]);
+
+Scene get_scene(float t)
+{
+    if (t <= times[0]) {
+        return scenes[0];
+    }
+    if (t >= times[NEVENTS - 1]) {
+        return scenes[NEVENTS - 1];
+    }
+
+    int i1, i2 = 1;
+    while (times[i2] <= t) {
+        i2++;
+    }
+    i1 = i2 - 1;
+
+    float alpha = (float)(t - times[i1]) / (float)(times[i2] - times[i1]);
+    fract8 b = floor(alpha * 256);
+    return Scene(scenes[i1], scenes[i2], b);
+}
+
+void render(CHSV &sky, CHSV &water, CHSV &ground)
+{
+
+    for (int i = 1; i <= 16; i++) {
+        leds[i] = ground;
+    }
+
+    for (int i = 26; i <= 41; i++) {
+        leds[i] = water;
+    }
+
+    for (int i = 50; i <= 65; i++) {
+        leds[i] = sky;
+    }
+}
+
+void render(Scene &scene)
+{
+    render(scene.sky, scene.water, scene.ground);
+}
+
 
 void setup()
 {
     FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NLEDS);
-
-	delay(500);
-    scene();
-    FastLED.show();
+	delay(100);
 }
 
 void loop() {
-	delay(500);
-}
+    static float t;
+    const float step = 0.1;
 
-void scene() {
-    const long sky = 0x59a8ff;
-    const long water = 0x1f604a;
-    const long ground = 0xFF8080;
+    Scene s = get_scene(t);
+    render(s);
+    FastLED.show();
 
-    leds[1]  = ground;
-    leds[2]  = ground;
-    leds[3]  = ground;
-    leds[2]  = ground;
-    leds[3]  = ground;
-    leds[4]  = ground;
-    leds[5]  = ground;
-    leds[6]  = ground;
-    leds[7]  = ground;
-    leds[8]  = ground;
-    leds[9]  = ground;
-    leds[10] = ground;
-    leds[11] = ground;
-    leds[12] = ground;
-    leds[13] = ground;
-    leds[14] = ground;
-    leds[15] = ground;
-    leds[16] = ground;
+    t += step;
+    if (t > times[NEVENTS - 1]) {
+        t -= times[NEVENTS - 1];
+    }
 
-    leds[26] = water;
-    leds[27] = water;
-    leds[28] = water;
-    leds[29] = water;
-    leds[30] = water;
-    leds[31] = water;
-    leds[32] = water;
-    leds[33] = water;
-    leds[32] = water;
-    leds[33] = water;
-    leds[34] = water;
-    leds[35] = water;
-    leds[36] = water;
-    leds[37] = water;
-    leds[38] = water;
-    leds[39] = water;
-    leds[40] = water;
-    leds[41] = water;
-
-    leds[50] = sky;
-    leds[51] = sky;
-    leds[52] = sky;
-    leds[53] = sky;
-    leds[54] = sky;
-    leds[55] = sky;
-    leds[56] = sky;
-    leds[57] = sky;
-    leds[58] = sky;
-    leds[59] = sky;
-    leds[60] = sky;
-    leds[61] = sky;
-    leds[62] = sky;
-    leds[63] = sky;
-    leds[64] = sky;
-    leds[65] = sky;
+    delay((long)round(step * 1000.0));
 }
